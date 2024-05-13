79760ed8a288d682a604520527c77b269b9057a7
ll-angela <57734848+ll-angela@users.noreply.github.com>
2024-05-12 19:17:33 -0500

    MapperCli Persona completo

--- cli-input-adapter/src/main/java/co/edu/javeriana/as/personapp/terminal/mapper/PersonaMapperCli.java
+++ cli-input-adapter/src/main/java/co/edu/javeriana/as/personapp/terminal/mapper/PersonaMapperCli.java
@@ -1,6 +1,7 @@
 package co.edu.javeriana.as.personapp.terminal.mapper;
 
 import co.edu.javeriana.as.personapp.common.annotations.Mapper;
+import co.edu.javeriana.as.personapp.domain.Gender;
 import co.edu.javeriana.as.personapp.domain.Person;
 import co.edu.javeriana.as.personapp.terminal.model.PersonaModelCli;
 
@@ -15,5 +16,25 @@ public class PersonaMapperCli {
 		personaModelCli.setGenero(person.getGender().toString());
 		personaModelCli.setEdad(person.getAge());
 		return personaModelCli;
+	}
+
+	public Person fromAdapterToDomain (PersonaModelCli personaModelCli) {
+		Person person = new Person();
+		person.setIdentification(personaModelCli.getCc());
+		person.setFirstName(personaModelCli.getNombre());
+		person.setLastName(personaModelCli.getApellido());
+		person.setAge(personaModelCli.getEdad());
+		switch (personaModelCli.getGenero().toLowerCase()) {
+			case "male":
+				person.setGender(Gender.MALE);
+				break;
+			case "female":
+				person.setGender(Gender.FEMALE);
+				break;
+			default:
+				person.setGender(Gender.OTHER);
+				break;
+		}
+		return person;
 	}
 }
